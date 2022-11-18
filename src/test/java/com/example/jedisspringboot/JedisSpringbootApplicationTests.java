96a379e15a7ece88021ee39ef7859d0f23099fdc
FrankLuo <FrankLuo666@gmail.com>
2022-11-18 17:09:32 +0900

    commit jedis code

---
+++ src/test/java/com/example/jedisspringboot/JedisSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jedisspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JedisSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
